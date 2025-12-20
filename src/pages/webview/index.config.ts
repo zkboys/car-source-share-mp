191c183d03006db3abe448d01d16000831e4487b
wangshubin <wang_sb@suixingpay.com>
2025-12-20 15:49:15 +0800

    :art: 整理代码

--- src/pages/webview/index.config.ts
+++ src/pages/webview/index.config.ts
@@ -1,7 +1,7 @@
 export default definePageConfig({
-    navigationStyle: 'custom',
-    navigationBarTitleText: '网页',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTextStyle: 'black',
+  navigationStyle: 'custom',
+  navigationBarTitleText: '',
+  navigationBarBackgroundColor: '#3689f5',
+  navigationBarTextStyle: 'white',
 })
 
--- src/pages/webview/index.tsx
+++ src/pages/webview/index.tsx
@@ -1,10 +1,11 @@
-import { WebView } from '@tarojs/components';
-import { useRouter } from '@tarojs/taro';
-import { useEffect, useState } from 'react';
+import {WebView} from '@tarojs/components';
+import {useRouter} from '@tarojs/taro';
+import {useEffect, useState} from 'react';
 
 export default function WebViewPage() {
   const router = useRouter();
-  const [url, setUrl] = useState('http://m.ktccar.cn/359232');
+  // const [url, setUrl] = useState('http://m.ktccar.cn/359232');
+  const [url, setUrl] = useState('http://localhost:5174/359232');
 
   useEffect(() => {
     // 如果通过路由参数传递了 URL，使用传递的 URL
@@ -13,6 +14,6 @@ export default function WebViewPage() {
     }
   }, [router.params]);
 
-  return <WebView src={url} />;
+  return <WebView src={url}/>
 }
 
